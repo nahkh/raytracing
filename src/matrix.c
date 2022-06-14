77fcf8a7c42d14085a4e9e16ba7278c8dd551243
Juuso Valli <juuso.valli@gmail.com>
2022-06-14 14:13:50 +0200

    Refactored matrix functions to always call inline matrix_make rather than creating memory layouts directly.

--- src/matrix.c
+++ src/matrix.c
@@ -34,20 +34,20 @@ Matrix matrix_multiply_matrix(const Matrix* m, const Matrix* n) {
     Vector c1 = matrix_column_1(n);
     Vector c2 = matrix_column_2(n);
     Vector c3 = matrix_column_3(n);
-    return (Matrix) {   vector_dot_product(&r1, &c1), vector_dot_product(&r1, &c2), vector_dot_product(&r1, &c3), 
+    return matrix_make( vector_dot_product(&r1, &c1), vector_dot_product(&r1, &c2), vector_dot_product(&r1, &c3), 
                         vector_dot_product(&r2, &c1), vector_dot_product(&r2, &c2), vector_dot_product(&r2, &c3), 
-                        vector_dot_product(&r3, &c1), vector_dot_product(&r3, &c2), vector_dot_product(&r3, &c3)};
+                        vector_dot_product(&r3, &c1), vector_dot_product(&r3, &c2), vector_dot_product(&r3, &c3));
 }
 
 Vector matrix_multiply_vector(const Matrix* m, const Vector* v) {
     Vector r1 = matrix_row_1(m);
     Vector r2 = matrix_row_2(m);
     Vector r3 = matrix_row_3(m);
-    return (Vector) {vector_dot_product(v, &r1), vector_dot_product(v, &r2), vector_dot_product(v, &r3)};
+    return vector_make(vector_dot_product(v, &r1), vector_dot_product(v, &r2), vector_dot_product(v, &r3));
 }
 
 Matrix matrix_multiply_scalar(const Matrix* m, double a) {
-    return (Matrix) {m->a1 * a, m->a2 * a, m->a3 * a, m->b1 * a, m->b2 * a, m->b3 * a, m->c1 * a, m->c2 * a, m->c3 * a};
+    return matrix_make(m->a1 * a, m->a2 * a, m->a3 * a, m->b1 * a, m->b2 * a, m->b3 * a, m->c1 * a, m->c2 * a, m->c3 * a);
 }
 
 
@@ -73,7 +73,7 @@ double matrix_determinant(const Matrix* m) {
     return m->a1 * (m->b2 * m->c3 - m->b3 * m->c2) + m->a2 * (m->b3 * m->c1 - m->b1 * m->c3) + m->a3 * (m->b1 * m->c2 - m->b2 * m->c1);
 }
 
-Matrix matrix_make(double a1, double a2, double a3, double b1, double b2, double b3, double c1, double c2, double c3) {
+inline Matrix matrix_make(double a1, double a2, double a3, double b1, double b2, double b3, double c1, double c2, double c3) {
     return (Matrix) {a1, a2, a3, b1, b2, b3, c1, c2, c3};
 }
 
